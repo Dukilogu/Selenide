3002eb1b44bf3e0e84e5bba79fa07ab2ef0a2922
Stefan Biryukov <stefanbiryukov2009@gmail.com>
2023-04-23 16:14:11 +0300

    Initial commit

---
+++ src/test/java/SelenideNegativeTest.java
@@ -0,0 +1,121 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideNegativeTest {
+
+    public static String setLocalDate(int day) {
+        String date = LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldTestForDay2() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(2));
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id='date'] .input__sub").shouldHave(Condition.text("Заказ на выбранную дату невозможен"));
+    }
+
+    @Test
+    void shouldTestForDay1() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(-1));
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=date] .input__sub").shouldHave(Condition.text("Заказ на выбранную дату невозможен"));
+    }
+
+    @Test
+    void shouldTestForEmptyCity() {
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(3));
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=city] .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldTestForEmptyDate() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=date] .input__sub").shouldHave(Condition.text("Неверно введена дата"));
+    }
+
+    @Test
+    void shouldTestForEmptyName() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(3));
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=name] .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldTestForLatinaName() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(3));
+        $("[data-test-id=name] .input__control").setValue("Pushkin Aleksandr");
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldTestForEmptyPhone() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(3));
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldTestForWrongPhone() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(3));
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=phone] .input__control").setValue("+7987872362443");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldTestForEmptyCheckBox() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(3));
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $(".button__content").click();
+        $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(Condition.text("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+
+    }
+}
---
+++ src/test/java/SelenideTest.java
@@ -0,0 +1,98 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTest {
+
+    public static String setLocalDate(int day) {
+        String date = LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return date;
+    }
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldTestForDay3() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(3));
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + setLocalDate(3)));
+    }
+
+    @Test
+    void shouldTestForDay4() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(4));
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + setLocalDate(4)));
+    }
+
+    @Test
+    void shouldTestForDay10() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(10));
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + setLocalDate(10)));
+    }
+
+    @Test
+    void shouldTestForDay365() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(365));
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + setLocalDate(365)));
+    }
+
+    @Test
+    void shouldTestForSelectFromList() {
+        $("[data-test-id=city] .input__control").setValue("Ор");
+        $$(".menu-item__control").find(Condition.exactText("Оренбург")).click();
+        $("[data-test-id=date] .input__control").doubleClick().sendKeys(setLocalDate(3));
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + setLocalDate(3)));
+    }
+
+    //тест про календарь не для проверки, он не доделан.
+    /*@Test
+    void shouldTestForWeekAhead() {
+        $("[data-test-id=city] .input__control").setValue("Оренбург");
+        $("[data-test-id=date] button").click();
+        $(".popup__content").find()
+        $("[data-test-id=name] .input__control").setValue("Пушкин Александр");
+        $("[data-test-id=phone] .input__control").setValue("+79878723624");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=notification] .notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + setLocalDate(10)));
+    }*/
+}
